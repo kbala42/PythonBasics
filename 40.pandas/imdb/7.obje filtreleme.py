c98905fce19f64c6abf6fa08f673162952e03815
kbala42 <kbala42@aim.com>
2022-04-05 07:32:09 +0300

    upt_05.04.2022

--- 40.pandas/imdb/7.obje filtreleme.py
+++ 40.pandas/imdb/7.obje filtreleme.py
@@ -1,19 +1,30 @@
 print('----------------------------------------------------------tw:@tek_elo')
 print('        imdb veritabanı\n')
-print('******* filtreleme *******\n')
+print('******* obje filtreleme *******\n')
 print('---------------------------------------------------------------------')
 print()
 import pandas as pd
 df = pd.read_csv('imdb_top_1000.csv')
+print(df.head())
 
-print('--------IMDB_Rating 8 den büyükleri yazdırma---------------')
+print('--------veri tabanı elaman tipleri---------------')
 df_filtered = df[df['IMDB_Rating'] >= 8]
+print(df.dtypes)
+
+print('--------Runtime nesnesi içinde 1. elemanları alıp tablonun sonuna ekliyoruz---------------')
+f = lambda x: (x["Runtime"].split())[0]
+df["RuntimeYeni"] = df.apply(f, axis=1)
+print(df['RuntimeYeni'])
+print(df.head())
+print(df.dtypes)
+
+print('--------Üretilen sütun yine object, burada filtreleme yapmak için int dönüştürüyoruz--------------')
+df['RuntimeYeni2'] = df['RuntimeYeni'].astype('int')
+print(df.dtypes)
+# şimdi yeni oluşan sütunda filtreleme yapabiliriz
+df_filtered = df.query('RuntimeYeni2 >= 140')
 print(df_filtered)
 
-print('--------IMDB_Rating 9.2 den büyükleri yazdırma---------------')
-df_filtered2 = df[df['IMDB_Rating'] >= 9.2]
-print(df_filtered2)
-
-print('--------IMDB_Rating 9.2 den büyükleri query metodu ile yazdırma---------------')
-df_filtered3 = df.query('IMDB_Rating >= 9.2')
-print(df_filtered3)
+print('--------RuntimeYeni nesnesini siliyoruz---------------')
+df = df.drop(['RuntimeYeni'], axis = 1)
+print(df.head())
